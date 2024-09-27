fad40d673b9e8f8272d3f5a5da681a933212d6fe
Gowtham Raju <gowthamraju239@gmail.com>
2024-09-27 16:30:05 +0530

    write logic for the calculator

---
+++ app.js
@@ -0,0 +1,97 @@
+// function add(a,b) {
+//     return a+b;
+// }
+
+// function subtract(a,b) {
+//     return a-b;
+// }
+
+// function mul(a,b) {
+//     return a*b;
+// }
+
+// function divide(a,b) {
+//     return a/b;
+// }
+
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+let values = "";
+let expression = document.querySelector(".num-op");
+
+// get the operators
+const getOperator = function(ope) {
+    operator = ope;
+    currentValue = values;
+    values = "";
+    expression.textContent = `${previousValue} ${operator}`;
+}
+
+// calculate the final result
+const calculate = function() {
+    previousValue = parseInt(previousValue)
+    currentValue = parseInt(currentValue)
+    let result;
+    switch(operator) {
+        case "+":
+            result = previousValue + currentValue;
+            break;
+        case "-":
+            result = previousValue - currentValue;
+            break;
+        case "*":
+            result = previousValue * currentValue;
+            break;
+        case "/":
+            result = previousValue / currentValue;
+            break;
+        default:
+            return;
+    }
+    userInput.textContent = result;
+    previousValue = "";
+    values = "";
+}
+
+
+let userInput = document.getElementById('user-input');
+
+// display the numbers
+const displayNumbers = function(nums) {
+    values += nums;
+    previousValue = values;
+    userInput.textContent = previousValue;
+}
+
+const numberBtns = document.querySelectorAll(".numbers");
+
+// numbers button
+numberBtns.forEach((numbers) => {
+    numbers.addEventListener("click", (e) => {
+        displayNumbers(e.target.textContent);
+    });
+});
+
+// operators button
+const operatorBtns = document.querySelectorAll(".operators");
+operatorBtns.forEach((op) => {
+    op.addEventListener("click", (e) => {
+        getOperator(e.target.textContent);
+    });
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", () => {
+    calculate();
+})
+
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    previousValue = "";
+    currentValue = "";
+    operator = "";
+    expression.textContent = "";
+    userInput.textContent = "";
+    
+})
